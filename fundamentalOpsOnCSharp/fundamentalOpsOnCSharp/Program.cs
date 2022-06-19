adbd5c9f4cff3acbd770b69f4b3a9d8466f8b2d3
toghrulvg <toghrulvg@code.edu.az>
2022-06-19 23:10:33 +0400

    operationsOnCSharp

---
+++ fundamentalOpsOnCSharp/fundamentalOpsOnCSharp/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1)Verilmish n deyishkeninin 3 ve 7 ye qaliqsiz bolunub bolunmemesini tapmaq.
+            //int n;
+            //n = 21;
+
+            //if (n % 3 == 0 && n % 7 == 0)
+            //{
+            //    Console.WriteLine("Eded 3 ve 7'e qaliqsiz bolunur");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("duzgun eded daxil edin");
+            //}
+
+            //2) n və m ədədləri verilir. Əgər n və m ədədləri hər ikisi cütdürsə n və m ədədlərinin cəmini hesablayın.
+            //int n;
+            //int m;
+            //m = 13;
+            //n = 4;
+
+            //if (n % 2 == 0 && m % 2 == 0)
+            //{
+            //    Console.WriteLine(n + m);
+            //}
+            //else Console.WriteLine("Ededlerden biri ve ya her ikisi tek ededdir");
+
+            //3) Verilmis n ve m (n<m) ededleri arasindaki tek ededlerin cemini tapin.
+            //int n = 3;
+            //int m = 11;
+            //int cem = 0;
+
+            //for (int i = n; i < m; i++)
+            //{
+            //    if (i % 2 == 0)
+            //        cem += i;
+            //}
+            //Console.WriteLine("verilmish n<m araligindaki cut ededlerin cemi=" + cem);
+
+
+
+
+            ////4)Verilmish arrayin icindeki tek ededlerin cemini tapin.
+            //int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //int cem = 0;
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (i % 2 != 0)
+            //        cem += arr[i];
+            //}
+
+            //Console.WriteLine("verilmish array'daki tek ededlerin cemi="
+            //                       + cem);
+
+
+            ////5) Verilmish arrayin icindeki cut ededlerin sayini tapin.
+            //int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //int cut = 0;
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (i % 2 == 0)
+            //        cut++;
+            //}
+            //Console.WriteLine("cut ededlerin sayi=" + cut);
+        }
+
+    }
+}
